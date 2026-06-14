16952437527b1ea4cc7d14f22085d416197ebced
Tu Nombre <tu_correo@example.com>
2026-06-13 20:18:42 -0500

    fix: importar ipcRenderer y contextBridge desde electron en preload

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,3 +1,5 @@
+import { ipcRenderer, contextBridge } from 'electron';
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - global puede no estar definido
 window.api = window.api || {};
